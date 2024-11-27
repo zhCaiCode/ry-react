7510abd27918e4af0011fa89fae145626a9bf15e
czh <2244356600@qq.com>
2024-11-27 16:32:30 +0800

    新增项目格式化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,13 @@ module.exports = {
     page: true,
     REACT_APP_ENV: true,
   },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': 'off', // 禁止使用console
+    'no-unused-vars': 'off', // 禁止定义未使用的变量
+    'no-debugger': 'error', // 禁止使用debugger
+    'no-var': 'error', // 要求使用 let 或 const 而不是 var
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
 };
